43a6bda724d7bdc22166b49049e758facf0b3b9c
lavg-8759 <l.vallejo.gonzalez@accenture.com>
2020-05-15 18:47:38 -0500

    added error message

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
  class QuizQuestion extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {incorrectAnswer: false};
+    }
+
     handleClick(buttonText)  {
         if(buttonText === this.props.quiz_question.answer) {
-            this.props.showNextQuestionHandler()
-        } 
+            this.props.showNextQuestionHandler();
+            this.setState( (state) => { return {incorrectAnswer: false}})
+        } else {
+            this.setState( (state) => { return {incorrectAnswer: true}})
+        }
     };
 
     render() {
@@ -16,6 +24,7 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 
         return (
             <main>
+                {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null }
                 <section>
                     <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
