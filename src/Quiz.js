849f687f011c32b9efea36092f71ae578031e282
lavg-8759 <l.vallejo.gonzalez@accenture.com>
2020-05-15 19:06:59 -0500

    setting quiz position to 1

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,7 +14,9 @@ class Quiz extends Component {
         this.setState( (state) => { return {quiz_position: state.quiz_position+1}})
     };
 
-    handleResetClick() {}
+    handleResetClick() {
+        this.setState( (state) => { return {quiz_position: 1}})
+    }
 
     render() {
         const isQuizEnd = this.state.quiz_position-1 === quizData.quiz_questions.length;
